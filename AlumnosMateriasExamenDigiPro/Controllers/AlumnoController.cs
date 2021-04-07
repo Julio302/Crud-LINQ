4b21f1a84a3f5b63a0a979f4c01759d800e64f3e
Julio302 <albertojuliofernandezgonzalez@gmail.com>
2021-04-07 18:23:34 -0500

    Crud con linq

---
+++ AlumnosMateriasExamenDigiPro/Controllers/AlumnoController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlumnosMateriasExamenDigiPro.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        [HttpGet]
+        public ActionResult GetAlumnos()
+        {
+            ML.Result  result = BL.Alumno.GetAll();
+            ML.Alumno alumno = new ML.Alumno();
+            alumno.Alumnos = result.Objects;
+            return View(alumno);
+        }
+
+        [HttpGet]
+        public ActionResult Form(int? IdAlumno)
+        {
+            ML.Alumno alumno = new ML.Alumno();
+            if (IdAlumno == null)
+            {
+                return View(alumno);
+            }
+            else
+            {
+                //Update
+                ML.Result result = BL.Alumno.GetByIdLQ(IdAlumno.Value);
+                if (result.Correct)
+                {
+                    alumno.IdAlumno = ((ML.Alumno)result.Object).IdAlumno;
+                    alumno.Nombre = ((ML.Alumno)result.Object).Nombre;
+                    alumno.ApellidoPaterno = ((ML.Alumno)result.Object).ApellidoPaterno;
+                    alumno.ApellidoMaterno = ((ML.Alumno)result.Object).ApellidoMaterno;
+                    return View(alumno);
+                }
+                else
+                {
+                    ViewBag.Message = result.ErrorMessage;
+                    return PartialView("validacion");
+                }
+            }
+        }
+
+        // Add
+        [HttpPost]
+        public ActionResult Form(ML.Alumno Alumno)
+        {
+            ML.Result result = new ML.Result();
+            if (Alumno.IdAlumno == 0)
+            {
+                result = BL.Alumno.Add(Alumno);
+                ViewBag.Message = "El alumno se agrego correctamente";
+            }
+            else
+            {
+                //update
+                result = BL.Alumno.Update(Alumno);
+                ViewBag.Message = "El alumno se modifico correctamente";
+            }
+            //si es incorrecto
+            if (!result.Correct)
+            {
+                ViewBag.Message = "No se pudo agregar el departamento";
+            }
+            return PartialView("validacion");
+        }
+
+        // GET: Alumno/Create
+        [HttpGet]
+        public ActionResult Delete(int? IdAlumno)
+        {
+            //delete
+            BL.Alumno.Delete(IdAlumno.Value);
+            ViewBag.Message = "El alumno se elimino correctamente";
+            return PartialView("validacion");
+        }
+
+        // POST: Alumno/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumno/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Alumno/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumno/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Alumno/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BL/Alumno.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DL;
+namespace BL
+{
+    public class Alumno
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (var db = new DL.JFernandezAlumnosDigiProEntities())
+                {
+                    var query = (from d in db.Alumnos
+                                 select new
+                                 {
+                                     d.idAlumno,
+                                     d.nombre,
+                                     d.apellidoPaterno,
+                                     d.apellidoMaterno
+                                 });
+
+                    result.Objects = new List<object>();
+                    if (query != null && query.ToList().Count > 0)
+                    {
+                        foreach (var item in query)
+                        {
+                            ML.Alumno oalumno = new ML.Alumno();
+                            oalumno.IdAlumno = item.idAlumno;
+                            oalumno.Nombre = item.nombre;
+                            oalumno.ApellidoPaterno = item.apellidoPaterno;
+                            oalumno.ApellidoMaterno = item.apellidoMaterno;
+                            result.Objects.Add(oalumno);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.ErrorMessage = "Error en " + e.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result GetByIdLQ(int IdAlumno)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (var bd = new DL.JFernandezAlumnosDigiProEntities())
+                {
+                    var query = (from p in bd.Alumnos.ToList()
+                                 where p.idAlumno == IdAlumno
+                                 select p).FirstOrDefault();
+
+                    if (query != null)
+                    {
+                        ML.Alumno oalumno = new ML.Alumno();
+                        oalumno.IdAlumno = query.idAlumno;
+                        oalumno.Nombre = query.nombre;
+                        oalumno.ApellidoPaterno = query.apellidoPaterno;
+                        oalumno.ApellidoMaterno = query.apellidoMaterno;
+                        result.Object = oalumno;
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.ErrorMessage = "Error en " + e.Message;
+            }
+            return result;
+        }
+        public static ML.Result Add(ML.Alumno Alumno)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (var bd = new DL.JFernandezAlumnosDigiProEntities())
+                {
+                    DL.Alumno oalumno = new DL.Alumno();
+                    oalumno.nombre = Alumno.Nombre;
+                    oalumno.apellidoPaterno = Alumno.ApellidoPaterno;
+                    oalumno.apellidoMaterno = Alumno.ApellidoMaterno;
+                    bd.Alumnos.Add(oalumno);
+                    bd.SaveChanges();
+                    result.Correct = true;
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.ErrorMessage = "Error en " + e.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result Update(ML.Alumno Alumno)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (var bd = new DL.JFernandezAlumnosDigiProEntities())
+                {
+                    var query = (from p in bd.Alumnos
+                                 where p.idAlumno == Alumno.IdAlumno
+                                 select p).First();
+
+                    if (query != null)
+                    {
+                        query.idAlumno = Alumno.IdAlumno;
+                        query.nombre = Alumno.Nombre;
+                        query.apellidoPaterno = Alumno.ApellidoPaterno;
+                        query.apellidoMaterno = Alumno.ApellidoMaterno;
+                        bd.SaveChanges();
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.ErrorMessage = "Error en " + e.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result Delete(int IdAlumno)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (var bd = new DL.JFernandezAlumnosDigiProEntities())
+                {
+                    var query = (from p in bd.Alumnos
+                                 where p.idAlumno == IdAlumno
+                                 select p).First();
+                    bd.Alumnos.Remove(query);
+                    bd.SaveChanges();
+                    result.Correct = true;
+                }
+            }
+            catch (Exception e)
+            {
+                result.Correct = false;
+                result.ErrorMessage = "Error en " + e.Message;
+            }
+            return result;
+        }
+    }
+}
